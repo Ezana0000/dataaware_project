847be594295193a3be73de361b8eb42a38c78fbd
Ezana0000 <137425744+Ezana0000@users.noreply.github.com>
2023-07-19 12:56:38 -0400

    Plan for project

--- Untitled.R
+++ Untitled.R
@@ -1,2 +1,29 @@
-hello
-hello
+# 1. SETUP ----
+## Set working directory ----
+
+## Load libraries ----
+
+# 2. DATA PROCESSING ----
+
+## Read in data ----
+
+## Process data ----
+
+# 3. FIT MODEL ----
+
+## Split dataset ----
+
+## Fit initial model ----
+
+## Tune model ----
+
+## Fit final model ----
+
+## Check accuracy ----
+
+# 4. FIGURES ---- 
+
+## Scatterplot of variables ----
+
+## Feature importance plot from random forest model ----
+
