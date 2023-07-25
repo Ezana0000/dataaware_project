6a14c898b812fc3634c2f8be451e3f59726012a6
Ezana0000 <137425744+Ezana0000@users.noreply.github.com>
2023-07-25 12:00:36 -0400

    Update main.R

--- main.R
+++ main.R
@@ -18,7 +18,8 @@ library(tidyverse)
 df <- load("df_combined.rda")
 
 # Drop the column named "column_to_drop"
-df_combined <- subset(df_combined, select = -person)
+df_combined <- df_combined %>%
+  mutate(class = as.factor(class))
 
 # Print the updated data frame
 print(df_combined)
@@ -26,13 +27,8 @@ print(df_combined)
 
 # 2. MODELING ----
 
-##
-print(df_combined)
-
 # Check data types of columns before conversion
-sapply(df_combined, class)
-
-
+# sapply(df_combined, class)
 
 ## Set Seed ----
 set.seed(123)
@@ -55,65 +51,92 @@ train_y <- unlist(train_y)
 test_y <- unlist(test_y)
 
 
-## Fit initial model ----
-library(randomForest)
+# ## Fit initial model ----
+# rf_model <- randomForest(
+#   x = train_x,
+#   y = train_y,
+#   xtest = test_x,
+#   ytest = test_y,
+#   importance = TRUE,
+#   ntree = 5000
+# )
+# 
+# # find the best value for the mtry hyperparameter. Set the x, y, xtest, ytest as before. Set the ntreeTry value to 500 (it will build 500 trees per try), stepFactor to 1.5, improve = 0.01, trace = TRUE, and plot = TRUE 
+# mtry <- tuneRF(
+#   x = train_x,
+#   y = train_y,
+#   xtest = test_x,
+#   ytest = test_y,
+#   ntreeTry = 5000,
+#   stepFactor = 1.5,
+#   improve = 0.01,
+#   trace = TRUE,
+#   plot = TRUE
+# )
+# 
+# # The code below will save the best value for the mtry and print it out
+# best.m <- mtry[mtry[, 2] == min(mtry[, 2]), 1]
+# print(mtry)
+# print(best.m)
+# 
+# ## Tune model ----
+# control <- trainControl(method='repeatedcv', 
+#                         number=10, 
+#                         repeats=3, 
+#                         search='grid')
+# #create tunegrid with 15 values from 1:15 for mtry to tunning model. Our train function will change number of entry variable at each split according to tunegrid. 
+# tunegrid <- expand.grid(.mtry = (1:15)) 
+# 
+# rf_gridsearch <- train(Class ~ ., 
+#                        data = df_combined,
+#                        method = 'rf',
+#                        metric = 'Accuracy',
+#                        tuneGrid = tunegrid)
+# print(rf_gridsearch)
 
-rf_model <- randomForest(
-  x = train_x,
-  y = train_y,
-  xtest = test_x,
-  ytest = test_y,
-  importance = TRUE,
-  ntree = 5000
-)
+# Create a vector of unique user IDs
+users_all <- unique(df_combined$person)
 
-# find the best value for the mtry hyperparameter. Set the x, y, xtest, ytest as before. Set the ntreeTry value to 500 (it will build 500 trees per try), stepFactor to 1.5, improve = 0.01, trace = TRUE, and plot = TRUE 
-mtry <- tuneRF(
-  x = train_x,
-  y = train_y,
-  xtest = test_x,
-  ytest = test_y,
-  ntreeTry = 5000,
-  stepFactor = 1.5,
-  improve = 0.01,
-  trace = TRUE,
-  plot = TRUE
-)
+# Initialize vectors for probabilities, predictions and actual class with NA
+prob_rf <- pred_rf <- actual <- rep(NA, length(users_all))
 
-# The code below will save the best value for the mtry and print it out
-best.m <- mtry[mtry[, 2] == min(mtry[, 2]), 1]
-print(mtry)
-print(best.m)
-
-## Tune model ----
-# Fit the random forest model with keep.forest = TRUE
-# Initialize an empty vector to store OOB errors
-rf_obb_errors <- c()
-
-# Fit the random forest model with keep.forest = TRUE and track OOB errors
-for (i in 1:3000) {
-  rf_model <- randomForest(
-    x = train_x,
-    y = train_y,
-    mtry = 7,
+# Loop over each unique user
+for (i in seq_along(users_all)) {
+ 
+  # Create a boolean mask for selecting data of the current user
+  indices_user_select <- df_combined$person == users_all[i]
+ 
+  # Train a Random Forest model using all data except for the current user's
+  mod <- randomForest(
+    class ~ hour, person, xyz.mean, xyz.absolute.deviation, xyz.standard.deviation, xyz.max.deviation, xyz.PSD.1, xyz.PSD.3, xyz.PSD.6, xyz.PSD.10, azimuth.mean, azimuth.absolute.deviation, azimuth.standard.deviation, azimuth.max.deviation, pitch.mean, pitch.absolute.deviation, pitch.standard.deviation, pitch.max.deviation, roll.mean, roll.absolute.deviation, roll.standard.deviation, roll.max.deviation, id,
+    data = df_combined[-indices_user_select,],
+    ntree = 1000,
     importance = TRUE,
-    ntree = i,
-    keep.forest = TRUE,
-    err.rate = TRUE
+    do.trace = 10
   )
-  # Append the OOB error to the vector
-  rf_oob_errors <- c(rf_obb_errors, rf_model$err.rate[nrow(rf_model$err.rate), 1])
+  
+ 
+  # Use the trained model to predict the class of the current user's data,
+  # then calculate the mean probability that the predicted class is "PD"
+  prob <- mean(predict(mod, df_combined[indices_user_select,], type = "response") == "PD")
+ 
+  # Create a prediction for the current user: if the calculated probability is
+  # more than 0.5, it predicts "PD"; otherwise, it predicts "Control"
+  pred <- ifelse(prob > 0.5, "PD", "Control")
+ 
+  # Store the calculated probability, prediction, and actual class for the current user
+  prob_rf[i] <- prob
+  pred_rf[i] <- pred
+  actual[i] <- unique(as.character(df_combined$class[indices_user_select]))
 }
 
-# Create a data frame to store training errors and iterations
-rf_res_df <- data.frame(TRAINING_ERROR = rf_oob_errors, ITERATION = 1:length(rf_oob_errors))
 
-# Find the iteration with the lowest training error
-best_nrounds <- rf_res_df %>%
-  filter(TRAINING_ERROR == min(TRAINING_ERROR)) %>%
-  pull(ITERATION)
 
-best_nrounds
+# Create a DataFrame with the calculated probabilities, the predictions,
+# and the actual classes for all unique users
+data.frame(prob_rf, pred_rf, actual)
+
+
 
 
 ## Fit final model ----
@@ -121,7 +144,7 @@ rf_final_model <-
   randomForest(
     x = train_x,
     y = train_y,
-    mtry = 7,
+    mtry = 11,
     importance = TRUE,
     ntree = 1000
   )
@@ -129,15 +152,6 @@ rf_final_model <-
 rf_final_model
 
 
-## Check accuracy ----
-# Use the final random forest model to make predictions on the test data
-predictions <- predict(rf_final_model, test_x)
-
-# Compare the predictions with the actual test labels (test_y)
-accuracy <- sum(predictions == test_y) / length(test_y)
-
-# Print the accuracy
-print(paste("Model Accuracy: ", round(accuracy * 100, 2), "%"))
 
 
 
